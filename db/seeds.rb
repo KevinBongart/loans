700a3c47ae1e2832444b141fe5428bf16c3e916e
Kevin Bongart <contact@kevinbongart.net>
2013-11-27 15:44:56 -0500

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+gringotts = Bank.create(name: "Gringotts")
+azkabank = Bank.create(name: "Azkabank")
+buckbank = Bank.create(name: "BuckBank")
+
+Loan.create(
+  name: "Hogwarts tuition",
+  monthly_payment: 112.05,
+  first_payment_at: 1.year.ago,
+  last_payment_at: 3.years.from_now,
+  bank: gringotts
+)
+
+Loan.create(
+  name: "Ollivanders' wand",
+  monthly_payment: 10,
+  first_payment_at: 8.months.ago,
+  last_payment_at: 3.months.from_now,
+  bank: gringotts
+)
+
+Loan.create(
+  name: "Broomstick",
+  monthly_payment: 29.99,
+  first_payment_at: 7.months.ago,
+  last_payment_at: 6.months.from_now,
+  bank: azkabank
+)
+
+Loan.create(
+  name: "Flying Ford Anglia",
+  monthly_payment: 63.12,
+  first_payment_at: 3.months.ago,
+  last_payment_at: 3.years.from_now,
+  bank: buckbank
+)
