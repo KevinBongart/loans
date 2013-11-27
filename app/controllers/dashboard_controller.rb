6616580f31da558388971484c68704f104232f5b
Kevin Bongart <contact@kevinbongart.net>
2013-11-27 13:33:38 -0500

    Better things

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
   include LoansHelper
 
   def index
-    @loans = Loan.includes(:bank).all
+    @loans = Loan.includes(:bank)
     @first_payment_at = @loans.map(&:first_payment_at).min
     @last_payment_at = @loans.map(&:last_payment_at).max
     @months = months_between(@first_payment_at, @last_payment_at)
