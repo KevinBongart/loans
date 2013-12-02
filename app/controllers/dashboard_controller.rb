9c49391962edd4cfbb86d543c6205b49d6235e2d
Kevin Bongart <contact@kevinbongart.net>
2013-12-02 13:44:33 -0500

    Visualize payments with Chart.js

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,9 +3,24 @@ class DashboardController < ApplicationController
 
   def index
     @loans = Loan.includes(:bank)
-    @first_payment_at = @loans.map(&:first_payment_at).min - 1.month
-    @last_payment_at = @loans.map(&:last_payment_at).max + 1.month
-    @months = months_between(@first_payment_at, @last_payment_at)
     @total_amount = @loans.sum(&:total_amount)
+
+    generate_month_list
+    generate_chart_data
+  end
+
+  private
+
+  def generate_month_list
+    first_payment_at = @loans.map(&:first_payment_at).min - 1.month
+    last_payment_at = @loans.map(&:last_payment_at).max + 1.month
+    @months = months_between(first_payment_at, last_payment_at).times.map do |nth_month|
+      first_payment_at + nth_month.months
+    end
+  end
+
+  def generate_chart_data
+    @month_names = @months.map { |month| month.strftime("%B %Y") }
+    @monthly_payments = @months.map { |month| total_for_month(@loans, month).to_f }
   end
 end
